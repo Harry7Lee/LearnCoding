51bf699bb5a7174cd9571ad5fff890356da550a9
HarryLee <perfect7lee@163.com>
2018-05-20 10:35:06 +0800

    ninja-chat completed

--- ninja-chat/src/components/chat.vue
+++ ninja-chat/src/components/chat.vue
@@ -3,11 +3,11 @@
     <h2 class="center teal-text">NINJA CHATS</h2>
     <div class="card">
       <div class="card-content">
-        <ul class="messages">
-          <li>
-            <span class="teal-text">Name</span>
-            <span class="grey-text text-darken-3">message</span>
-            <span class="teal-text time">time</span>
+        <ul class="messages" v-chat-scroll>
+          <li v-for="message in messages" :key="message.id">
+            <span class="teal-text">{{message.name}}:</span>
+            <span class="grey-text text-darken-3">{{message.content}}</span>
+            <span class="grey-text time">{{message.timestamp}}</span>
           </li>
         </ul>
       </div>
@@ -20,6 +20,9 @@
 
 <script>
 import newMessage from "@/components/newMessage";
+import db from "@/firebase/init";
+import moment from "moment";
+
 export default {
   name: "chat",
   props: ["name"],
@@ -27,7 +30,25 @@ export default {
     newMessage
   },
   data() {
-    return {};
+    return {
+      messages: []
+    };
+  },
+  created() {
+    let ref = db.collection("messages").orderBy("timestamp");
+    ref.onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        if (change.type == "added") {
+          let doc = change.doc;
+          this.messages.push({
+            id: doc.id,
+            name: doc.data().name,
+            content: doc.data().content,
+            timestamp: moment(doc.data().timestamp).format("lll")
+          });
+        }
+      });
+    });
   }
 };
 </script>
@@ -41,7 +62,23 @@ export default {
   font-size: 1.4em;
 }
 .chat .time {
-  font-size: 1.2em;
+  font-size: 0.8em;
   display: block;
+}
+.messages {
+  max-height: 300px;
+  overflow: auto;
+}
+
+.messages::-webkit-scrollbar {
+  width: 3px;
+}
+
+.messages::-webkit-scrollbar-track {
+  background: #ddd;
+}
+
+.messages::-webkit-scrollbar-thumb {
+  background: #aaa;
 }
 </style>
--- ninja-chat/src/components/newMessage.vue
+++ ninja-chat/src/components/newMessage.vue
@@ -1,24 +1,44 @@
 <template>
-    <div class="new-message">
-        <form @submit.prevent="addMessage">
-            <label for="new-message">New Message (enter to add)</label>
-            <input type="text" name="new-message" v-model="newMessage">
-        </form>
+  <div class="new-message">
+    <form @submit.prevent="addMessage">
+      <label for="new-message">New Message (enter to add)</label>
+      <input type="text" name="new-message" v-model="newMessage">
+      <p class="red-text" v-if="feedback">{{feedback}}</p>
+    </form>
 
-    </div>
+  </div>
 </template>
 
 <script>
+import db from "@/firebase/init";
 export default {
   name: "newMessage",
   props: ["name"],
   data() {
     return {
-      newMessage: null
+      newMessage: null,
+      feedback: null
     };
   },
   methods: {
-    addMessage() {}
+    addMessage() {
+      if (this.newMessage) {
+        let ref = db
+          .collection("messages")
+          .add({
+            name: this.name,
+            content: this.newMessage,
+            timestamp: Date.now()
+          })
+          .catch(err => {
+            console.log(err);
+          });
+        this.newMessage = null;
+        this.feedback = null;
+      } else {
+        this.feedback = "You must enter a message in order to send one!";
+      }
+    }
   }
 };
 </script>
