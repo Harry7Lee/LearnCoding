3c9cf967fa60b8020e672ccccdcad8051fc43512
HarryLee <perfect7lee@163.com>
2018-05-20 22:37:03 +0800

    move check alias to cloud function

--- geo-ninjas/src/components/auth/Signup.vue
+++ geo-ninjas/src/components/auth/Signup.vue
@@ -1,32 +1,34 @@
 <template>
-    <div class="signup container">
-        <form @submit.prevent="signup" class="card-panel">
-            <h2 class="center deep-purple-text"> </h2>
-            <div class="field">
-                <label for="email">Email:</label>
-                <input type="email" name="email" v-model="email">
-            </div>
-            <div class="field">
-                <label for="password">Password:</label>
-                <input type="password" name="password" v-model="password">
-            </div>
-            <div class="field">
-                <label for="alias">Alias:</label>
-                <input type="text" name="alias" v-model="alias">
-            </div>
-            <p class="red-text center" v-if="feedback">{{feedback}}</p>
-            <div class="field center">
-                <button class="btn btn-large deep-purple">Sign Up</button>
-            </div>
+  <div class="signup container">
+    <form @submit.prevent="signup" class="card-panel">
+      <h2 class="center deep-purple-text"> </h2>
+      <div class="field">
+        <label for="email">Email:</label>
+        <input type="email" name="email" v-model="email">
+      </div>
+      <div class="field">
+        <label for="password">Password:</label>
+        <input type="password" name="password" v-model="password">
+      </div>
+      <div class="field">
+        <label for="alias">Alias:</label>
+        <input type="text" name="alias" v-model="alias">
+      </div>
+      <p class="red-text center" v-if="feedback">{{feedback}}</p>
+      <div class="field center">
+        <button class="btn btn-large deep-purple">Sign Up</button>
+      </div>
 
-        </form>
-    </div>
+    </form>
+  </div>
 </template>
 
 <script>
 import slugify from "slugify";
 import db from "@/firebase/init";
 import firebase from "firebase";
+import functions from "firebase/functions";
+
 export default {
   name: "Signup",
   data() {
@@ -41,36 +43,35 @@ export default {
   methods: {
     signup() {
       if (this.alias && this.email && this.password) {
-        this.slug = slugify(this.alias, {
-          replacement: "-",
-          remove: /[$*_~+.()'"\-:@]/g,
-          lower: true
-        });
-        let ref = db.collection("users").doc(this.slug);
-        ref.get().then(doc => {
-          if (doc.exists) {
-            this.feedback = "This alias already exists!";
-          } else {
-            firebase
-              .auth()
-              .createUserWithEmailAndPassword(this.email, this.password)
-              .then(cred => {
-                ref.set({
-                  alias: this.alias,
-                  geolocation: null,
-                  user_id: cred.user.uid
-                });
-              })
-              .then(() => this.$router.push({ name: "GMap" }))
-              .catch(err => {
-                console.log(err);
-                this.feedback = err.message;
-              });
-          }
-        });
       } else {
-        this.feedback = "Please enter all fields!";
       }
+      this.slug = slugify(this.alias, {
+        replacement: "-",
+        remove: /[$*_~+.()'"\-:@]/g,
+        lower: true
+      });
+      let checkAlias = firebase.functions().httpsCallable("checkAlias");
+      checkAlias({ slug: this.slug }).then(result => {
+        if (!result.data.unique) {
+          this.feedback = "This alias already exists!";
+        } else {
+          firebase
+            .auth()
+            .createUserWithEmailAndPassword(this.email, this.password)
+            .then(cred => {
+              ref.set({
+                alias: this.alias,
+                geolocation: null,
+                user_id: cred.user.uid
+              });
+            })
+            .then(() => this.$router.push({ name: "GMap" }))
+            .catch(err => {
+              console.log(err);
+              this.feedback = err.message;
+            });
+        }
+      });
     }
   }
 };
